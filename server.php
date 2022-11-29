9ed30978902495e8e199b8d73f9c3b0de379711f
Smatzondi <105789002+Smatzondi@users.noreply.github.com>
2022-11-29 10:40:54 +0100

    Add files via upload

---
+++ server.php
@@ -0,0 +1,31 @@
+<?php
+require_once "database/config.php";
+
+include_once "database/queries/tables.php";
+require "database/queries/users.php";
+
+
+
+
+// $firstname = "oluwasegun";
+// $lastname = "AWETOBA";
+// $username = "smatzondi";
+// $email = "bigboycomrade@gmail.com";
+// $password = "12345678";
+// $userquery = "INSERT INTO users (username, password, email, firstname, lastname) VALUES ('$username', '$password','$email', '$firstname', '$lastname')"; 
+// mysqli_query($db, $tableUsers) or exit(mysqli_error($db));
+// mysqli_query($db, $userquery) or exit(mysqli_error($db));
+
+// $user_id = 56;
+// $description = "sold old TV";
+// $price = 1234567890;
+// $profitquery = "INSERT INTO profit (user_id, description, price) VALUES ('$user_id','$description','$price')";
+// mysqli_query($db, $tableProfit) or exit(mysqli_error($db));
+// mysqli_query($db, $profitquery) or exit(mysqli_error($db));
+
+// $user_id = 56;
+// $description = "bought fuel";
+// $price = 123456789;
+// $lossquery = "INSERT INTO loss (user_id, description, price) VALUES ('$user_id','$description','$price')";
+// mysqli_query($db, $tableLoss) or exit(mysqli_error($db));
+// mysqli_query($db, $lossquery) or exit(mysqli_error($db));
